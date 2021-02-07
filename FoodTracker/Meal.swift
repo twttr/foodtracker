8a47e9f4e40f1ef4b459f4246a8a04afc28d80d8
Pavel Romanishkin <me@twttr.io>
2021-02-07 16:26:24 +0100

    save and load meals

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    //MARK: Archiving Paths
+    static let documentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentsDirectory.appendingPathComponent("meals")
+    
+    //MARK: Types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     //MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
@@ -26,6 +38,24 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+    }
+
+    //MARK: NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(photo, forKey: PropertyKey.photo)
+        coder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder: NSCoder) {
+        guard let name = coder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
         
+        let photo = coder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = coder.decodeInteger(forKey: PropertyKey.rating)
+        
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -17,8 +17,12 @@ class MealTableViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = editButtonItem
-
-        loadSampleMeals()
+        
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        } else {
+            loadSampleMeals()
+        }
     }
 
     // MARK: - Table view data source
@@ -52,6 +56,7 @@ class MealTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             meals.remove(at: indexPath.row)
+            saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -110,6 +115,7 @@ class MealTableViewController: UITableViewController {
                 meals.append(meal)
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
+            saveMeals()
         }
     }
     
@@ -127,4 +133,26 @@ class MealTableViewController: UITableViewController {
         meals += [meal1, meal2, meal3]
     }
 
+    private func saveMeals() {
+        if let dataToBeArchived = try? NSKeyedArchiver.archivedData(withRootObject: meals, requiringSecureCoding: false) {
+            do {
+                try dataToBeArchived.write(to: Meal.archiveURL)
+                os_log("Meals successfully saved", log: OSLog.default, type: .debug)
+            } catch {
+                os_log("Failed to save meals", log: OSLog.default, type: .error)
+            }
+        }
+    }
+    
+    private func loadMeals() -> [Meal]? {
+        guard let archivedData = try? Data(contentsOf: Meal.archiveURL) else {
+            os_log("Failed to load archived meals", log: OSLog.default, type: .error)
+            return []
+        }
+        guard let savedMeals = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(archivedData) as? [Meal] else {
+            os_log("Failed to load archived meals", log: OSLog.default, type: .error)
+            return []
+        }
+        return savedMeals
+    }
 }
