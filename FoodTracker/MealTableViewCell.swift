bd768a5d4183352a38aa4cfc420bd9f6a621b6ed
Pavel Romanishkin <me@twttr.io>
2021-02-07 12:42:22 +0100

    add meal table view cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Pavel Romanishkin on 07.02.21.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
